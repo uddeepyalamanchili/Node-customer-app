74c08e6db585049c0d8a9fceefa536a03c40ee81
uddip_yalamanchili <tinku@localhost.localdomain>
2020-12-09 22:28:30 +0530

    minor changes

--- model/student-mysql.js
+++ model/student-mysql.js
@@ -4,8 +4,8 @@ var pool  = mysql.createPool({
   connectionLimit : 10,
   host            : 'localhost',
   user            : 'root',
-  password        : 'yup123456',
-  database        : 'nodejs'
+  password        : '',
+  database        : ''
 });
 
 var service = {};
